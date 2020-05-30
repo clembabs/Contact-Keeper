8eea6bd510cbb62a01f9e847d1c89d9a08fecc99
clembabs <clembabs10@gmail.com>
2020-05-30 02:47:40 +0100

    initial commit

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h1>About This App</h1>
+            <p className="my-1">
+            This is a Full Stack React app for keeping Contacts
+            </p>
+        </div>
+    )
+}
+
+export default About;
